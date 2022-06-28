6d357102f273a3265400f336935b014ab242e9e1
mohamednegm <67514300+BlackYun2@users.noreply.github.com>
2022-06-28 05:20:01 +0200

    fixed some minor issues                            like removed items stay removed after you reterun in cart the number stopped starting from 0 with arrows

--- src/app/components/plist/plist.component.ts
+++ src/app/components/plist/plist.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { GdataService } from '../../services/gdata.service';
 import { Component, OnInit } from '@angular/core';
 import { product } from '../../models/product';
@@ -10,13 +11,24 @@ import { product } from '../../models/product';
 export class PlistComponent implements OnInit {
 
   products: product[] = [];
-  constructor(private data: GdataService) { }
+  constructor(private data: GdataService, private router: Router) { }
 
   ngOnInit(): void {
+    this.dontCareProducts();
     this.data.getdata().subscribe(x => { this.products = x; });
   }
   removeproduct(x: product) {
-    this.products = this.products.filter(p => p.id !== x.id);
+    this.data.removeLocaly = [(x.id)];
+    this.dontCareProducts();
+  }
+  isproduct(x: number): boolean {
+    return !this.data.removeLocaly.includes(x);
+  }
+  dontCareProducts() {
+    this.data.removeLocaly.forEach(e => {
+      this.products = this.products.filter(p => p.id !== e);
+    });
+    console.log(this.products);
   }
 
 
--- src/app/services/gdata.service.ts
+++ src/app/services/gdata.service.ts
@@ -7,9 +7,16 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class GdataService {
+  _removeLocaly: number[] = [];
 
   constructor(private http: HttpClient) { }
   getdata(): Observable<product[]> {
     return this.http.get<product[]>("assets/data.json");
+  }
+  get removeLocaly() {
+    return this._removeLocaly;
+  }
+  set removeLocaly(x) {
+    this._removeLocaly.push(Number(x));
   }
 }
